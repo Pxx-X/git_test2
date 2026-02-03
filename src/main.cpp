e548c161e786aec91bee5283d1a6e9f525bb4e53
Pxx-X <17875200128@163.com>
2026-02-03 17:32:17 +0800

    mdf:user1

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ using namespace std;
  * @return Sum of a and b.
  */
 int add(int a, int b) { return a + b; }
-
+int multiply(int a, int b) { return a * b; }
 int main() {
-    cout << "Hello from user1! 2 + 4 = " << add(2, 4) << '\n';
-    return 0;
+  cout << "Hello from user1! 2 + 4 = " << add(2, 4) << '\n';
+  cout << "Hello from user1! 2 * 4 = " << multiply(2, 4) << '\n';
+  return 0;
 }
