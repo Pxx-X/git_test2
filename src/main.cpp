94615eb04a796e99634d3bb8037569d02b5c775b
Pxx-X <17875200128@163.com>
2026-02-03 20:24:10 +0800

    user1:add divide

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,11 @@ using namespace std;
 int add(int a, int b) { return a + b; }
 int multiply(int a, int b) { return a * b; }
 int subtract(int a, int b) { return a - b; }
+int divide(int a, int b) { return a / b; }
 int main() {
   cout << "Hello from user1! 22 + 4 = " << add(22, 4) << '\n';
   cout << "Hello from user1! 22 * 4 = " << multiply(22, 4) << '\n';
   cout << "Hello from user1! 4 - 2 = " << subtract(4, 2) << '\n';
+  cout << "Hello from user1! 20 / 5 = " << divide(20, 5) << '\n';
   return 0;
 }
