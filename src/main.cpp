6dfd601fa0b13fa28cc68c001ccdc6cd6a2c9b1e
Pxx-X <17875200128@163.com>
2026-02-03 19:11:07 +0800

    add: subtract

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@ using namespace std;
  */
 int add(int a, int b) { return a + b; }
 int multiply(int a, int b) { return a * b; }
+int subtract(int a, int b) { return a - b; }
 int main() {
   cout << "Hello from user1! 22 + 4 = " << add(22, 4) << '\n';
   cout << "Hello from user1! 22 * 4 = " << multiply(22, 4) << '\n';
+  cout << "Hello from user1! 4 - 2 = " << subtract(4, 2) << '\n';
   return 0;
 }
